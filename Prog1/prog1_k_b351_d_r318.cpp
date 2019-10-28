606127bc106add068fff92637194bc549d4377d1
KimberlyRayeBeal <57076886+KimberlyRayeBeal@users.noreply.github.com>
2019-10-27 19:09:03 -0500

    Update prog1_k_b351_d_r318.cpp

--- Prog1/prog1_k_b351_d_r318.cpp
+++ Prog1/prog1_k_b351_d_r318.cpp
@@ -1,41 +1,16 @@
 /*
    File: prog1_k_b351_d_r318.cpp
 
-   Author: Kimberly Beal and Devin Roseman
+   Author: Kimberly Beal
    C.S.1428.751
    Lab Section: L52
    Program: #1
    Due Date: 07/16/18
 
-   This program prompts the user for the diameter of a sphere (in inches)
-   allowing for real input data. After reading the value entered at the
-   keyboard, the program calculates the radius of the sphere. It then
-   calculates the volume of the sphere. Two blank lines are displayed
-   before the author's identifying information is written to the console.
-   One blank line is written to the console before the diameter of the
-   sphere along with the calculated radius and volume of the sphere are
-   written in sentence form. Numeric values in the output statement are
-   displayed to one decimal place. The layout and content of the output
-   screen are shown below.
-
-   Input (keyboard):  diameter  (double)
-   Constants:         PI = 3.141592 (double)
-   Output (console):
-              Author's Name
-              C.S.1428.?        // '?' represents student's lecture section #
-              Lab Section: L?   // '?' represents student's lab section #
-              --/--/--          // dashes represent due date, month/day/year
-                  <blank line>
-              A sphere with a diameter of ? inches
-              and a radius of ? inches has a volume
-              of ? cubic inches.
-
-   ================================================
-
-   <Output will vary based on actual input values.>
-
-
-
+   This program prompts the user for the diameter of a sphere (in inches), 
+   the program calculates the radius of the sphere, then
+   calculates the volume of the sphere. The the diameter, radius and 
+   volume are output to the screen.
 */
 #include <iostream>
 #include <cmath>
@@ -65,8 +40,8 @@ int main()
 
     cout << endl << endl;
 
-    cout << "Kimberly Beal and Devin Roseman" << endl
-         << "C.S.1428.751" << endl
+    cout << "Kimberly Beal" << endl
+         << "C.S.1428" << endl
          << "Lab Section: L52" << endl
          << "07-16-18" << endl << endl;
 
